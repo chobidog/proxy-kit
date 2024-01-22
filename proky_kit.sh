f251d940f20ef1dcc4a1c6750ce9c1bc601dfcd9
ReiF <33890453+chobidog@users.noreply.github.com>
2024-01-22 17:37:47 +0900

    Update proky_kit.sh

--- proky_kit.sh
+++ proky_kit.sh
@@ -2,8 +2,8 @@ function set_proxy {
     local ssid=$(networksetup -getairportnetwork en0 | awk -F: '{print $2}' | sed -e 's/^[[:space:]]*//')
     
     if [[ "$ssid" == "KIT-WLAP2" ]]; then
-        export http_proxy="http://wwwproxy.kanazawa-it.ac.jp:8080"
-        export https_proxy="http://wwwproxy.kanazawa-it.ac.jp:8080"
+        export http_proxy="***************:****" #*** のところは適宜設定しなければいけない
+        export https_proxy="***************:****"
     else
         unset http_proxy
         unset https_proxy
